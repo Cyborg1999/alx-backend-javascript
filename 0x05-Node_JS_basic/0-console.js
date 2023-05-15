3e697f5574008aedd5c89fbe9244c1db8825b17d
Cyborg1999 <andrewk.tonui@gmail.com>
2023-05-15 17:27:50 +0300

    Express

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The Message to display
+ * @author Andrew Tonui <https://github.com/Cyborg1999>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
